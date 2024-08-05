115bba5ceba9e137c06c974d4ce2b87cd73b242e
unknown <vishaljagdeeshbind@gmail.com>
2024-08-05 13:29:36 +0530

    Data Transformation

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -4,8 +4,11 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from src.mlproject.utils import read_sql_data
+
 from sklearn.model_selection import train_test_split
+
 from dataclasses import dataclass
+
 
 @dataclass
 class DataIngestionConfig:
@@ -18,22 +21,27 @@ class DataIngestion:
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        try :
-            ##reading data from mysql
-            df=read_sql_data()
-            logging.info("reading Complete mysql database")
+        try:
+            ##reading the data from mysql
+            df=pd.read_csv(os.path.join('notebook/data','raw.csv'))
+            logging.info("Reading completed mysql database")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            
+
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            train_set(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Data Ingestion is Completed")
+            logging.info("Data Ingestion is completed")
+
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+
+
             )
+
+
         except Exception as e:
             raise CustomException(e,sys)
