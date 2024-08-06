c44787fafa216742d5225edeef7fd64bb9f6e999
unknown <vishaljagdeeshbind@gmail.com>
2024-08-06 12:23:54 +0530

    mlflow

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## End To End Data Science Project
+
+import dagshub
+dagshub.init(repo_owner='vishaljbind', repo_name='DSPROJECT', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+mlflow.log_param('parameter name', 'value')
+mlflow.log_metric('metric name', 1)
--- src/mlproject/components/model_tranier.py
+++ src/mlproject/components/model_tranier.py
@@ -107,44 +107,44 @@ class ModelTrainer:
             print("This is the best model:")
             print(best_model_name)
 
-            # model_names = list(params.keys())
+            model_names = list(params.keys())
 
-            # actual_model=""
+            actual_model=""
 
-            # for model in model_names:
-            #     if best_model_name == model:
-            #         actual_model = actual_model + model
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
 
-            # best_params = params[actual_model]
+            best_params = params[actual_model]
 
-            # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/mlprojecthindi.mlflow")
-            # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            mlflow.set_registry_uri("https://dagshub.com/vishaljbind/DSPROJECT.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-            # # mlflow
+            # mlflow
 
-            # with mlflow.start_run():
+            with mlflow.start_run():
 
-            #     predicted_qualities = best_model.predict(X_test)
+                predicted_qualities = best_model.predict(X_test)
 
-            #     (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
 
-            #     mlflow.log_params(best_params)
+                mlflow.log_params(best_params)
 
-            #     mlflow.log_metric("rmse", rmse)
-            #     mlflow.log_metric("r2", r2)
-            #     mlflow.log_metric("mae", mae)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
 
 
-            #     # Model registry does not work with file store
-            #     if tracking_url_type_store != "file":
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
 
-            #         # Register the model
-            #         # There are other ways to use the Model Registry, which depends on the use case,
-            #         # please refer to the doc for more information:
-            #         # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            #         mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
-            #     else:
-            #         mlflow.sklearn.log_model(best_model, "model")
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
 
 
 
